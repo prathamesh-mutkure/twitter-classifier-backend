d14b622883e0a5b2fd2429fb8f6aca1cc5d71cf1
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-04-17 16:30:22 +0530

    Added Account and Hashtag APIs

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
+import tweepy
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+
+BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
+client = tweepy.Client(bearer_token=BEARER_TOKEN)
 
 
 @app.route("/")
@@ -12,6 +16,49 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+def get_tweets_by_query(query: str, limit):
+    tweets_raw = tweepy.Paginator(client.search_recent_tweets,
+                                  query=query,
+                                  tweet_fields=['context_annotations', 'created_at'],
+                                  max_results=limit
+                                  ).flatten(limit=4)
+
+    tweets = []
+
+    for tweet in tweets_raw:
+        tweets.append({
+            "id": tweet.id,
+            "text": tweet.text,
+        })
+
+    return tweets
+
+
+@app.route("/tweets/username", methods=['POST'])
+def load_tweet_from_username():
+    data = request.json
+    username = data['username']
+    limit = data['limit']
+
+    query = f'from:{username}'
+
+    tweets = get_tweets_by_query(query, limit)
+
+    return tweets
+
+
+@app.route("/tweets/hashtag", methods=['POST'])
+def load_tweet_from_hashtag():
+    data = request.json
+    hashtag = data['hashtag']
+    limit = data['limit']
+
+    query = f'#{hashtag}'
+
+    tweets = get_tweets_by_query(query, limit)
+
+    return tweets
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
